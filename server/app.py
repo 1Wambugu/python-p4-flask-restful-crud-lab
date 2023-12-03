517fb6253127430e05b9b2ca3af6338a67c757b7
1Wambugu <martingakuya@gmail.com>
2023-12-03 11:37:08 +0300

    python-p4-flask-restful-crud-lab

--- server/app.py
+++ server/app.py
@@ -9,7 +9,6 @@ from models import db, Plant
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
@@ -21,7 +20,7 @@ class Plants(Resource):
 
     def get(self):
         plants = [plant.to_dict() for plant in Plant.query.all()]
-        return make_response(jsonify(plants), 200)
+        return make_response(jsonify(plants=plants), 200)
 
     def post(self):
         data = request.get_json()
@@ -35,19 +34,40 @@ class Plants(Resource):
         db.session.add(new_plant)
         db.session.commit()
 
-        return make_response(new_plant.to_dict(), 201)
+        return make_response(jsonify(plant=new_plant.to_dict()), 201)
 
+    def patch(self, id):
+        plant = Plant.query.get(id)
 
-api.add_resource(Plants, '/plants')
+        if not plant:
+            return make_response(jsonify(error="Plant not found"), 404)
 
+        data = request.get_json()
 
+        if 'is_in_stock' in data:
+            plant.is_in_stock = data['is_in_stock']
+
+        db.session.commit()
+
+        return make_response(jsonify(plant=plant.to_dict()), 200)
+
+    def delete(self, id):
+        plant = Plant.query.get(id)
+
+        if not plant:
+            return make_response(jsonify(error="Plant not found"), 404)
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        return make_response('', 204)
+    
 class PlantByID(Resource):
-
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
-
+api.add_resource(Plants, '/plants')
 api.add_resource(PlantByID, '/plants/<int:id>')
 
 
